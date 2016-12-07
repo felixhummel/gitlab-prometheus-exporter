053c60c419f4af16dcfe931e1728aae1b83855ae
Felix Hummel <felix@felixhummel.de>
2016-12-07 18:19:03 +0100

    initial

---
+++ README.rst
@@ -0,0 +1,52 @@
+**************************
+Prometheus Gitlab Exporter
+**************************
+This uses `Gitlab <https://gitlab.com>`__'s API to fetch some data and exposes
+it to `Prometheus <https://prometheus.io/>`__.
+
+
+Getting Started
+===============
+::
+
+    ./gitlab_exporter.py
+
+::
+
+    curl localhost:3001
+
+
+Config
+======
+The following environment variables may be set::
+
+    LOGLEVEL=INFO PORT=3002 INTERVAL=180 ./gitlab_exporter.py
+
+====================  ===========
+Environment Variable  Description
+====================  ===========
+LOGLEVEL              Standard Python `log level`_
+PORT                  Port to listen on
+INTERVAL              Interval in seconds to wait between data fetches
+====================  ===========
+
+.. _log level: https://docs.python.org/3.5/library/logging.html#levels
+
+
+Notes
+=====
+Please note that this fetches **all** projects. This may take a while and
+produces load on your Gitlab instance. For 300 projects it took 10 seconds on
+my server.
+
+
+Gitlab API
+----------
+- REST API Reference https://github.com/gitlabhq/gitlabhq/tree/master/doc/api
+- Python API http://python-gitlab.readthedocs.io/en/stable/api-usage.html
+
+
+Prometheus Exporter
+-------------------
+https://github.com/prometheus/client_python
+
---
+++ gitlab_exporter.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import logging
+import os
+import time
+
+import gitlab
+from prometheus_client import start_http_server, Gauge
+
+try:
+    loglevel = getattr(logging, os.environ.get('LOGLEVEL', 'WARN').upper())
+except AttributeError:
+    pass
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(level=loglevel)
+
+# time to sleep between calls to gitlab
+INTERVAL = int(os.environ.get('INTERVAL', 300))
+
+# port to listen on
+PORT = int(os.environ.get('PORT', 3001))
+
+# This expects a valid config with default server. See
+# http://python-gitlab.readthedocs.io/en/stable/cli.html#configuration
+gl = gitlab.Gitlab.from_config()
+
+sidekiq_jobs_enqueued = Gauge('sidekiq_jobs_enqueued', 'Total number of enqueued Sidekiq jobs')
+sidekiq_jobs_failed = Gauge('sidekiq_jobs_failed', 'Total number of failed Sidekiq jobs')
+sidekiq_jobs_processed = Gauge('sidekiq_jobs_processed', 'Total number of processed Sidekiq jobs')
+gitlab_project_count = Gauge('gitlab_project_count', 'Number of projects')
+
+
+def get_stats():
+    log.info('fetching projects')
+    projects = gl.projects.all(all=True)
+    x = gl.sidekiq.job_stats()
+    # {'jobs': {'enqueued': 0, 'failed': 140300, 'processed': 211394}}
+    sidekiq_jobs_enqueued.set(x['jobs']['enqueued'])
+    sidekiq_jobs_enqueued.set(x['jobs']['failed'])
+    sidekiq_jobs_enqueued.set(x['jobs']['processed'])
+    gitlab_project_count.set(len(projects))
+
+
+if __name__ == '__main__':
+    start_http_server(PORT)
+    log.info('listening on port {0}'.format(PORT))
+    while True:
+        try:
+            get_stats()
+            log.info('sleeping for {0} seconds'.format(INTERVAL))
+            time.sleep(INTERVAL)
+        except KeyboardInterrupt:
+            break
